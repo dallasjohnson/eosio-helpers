b1fbf5bf6162d82d4b89fc1402a706647bda6c39
Dallas Johnson <dallasj001@gmail.com>
2018-05-11 09:10:02 +0100

    Improve the run.sh sample script for matching eosio.token

--- 3.0/sample_scripts/run.sh
+++ 3.0/sample_scripts/run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-source eosio_helpers_common
+token="EDB"
 
 color_printf "Create a new currency"
-exe cleos push action ${ACCOUNT_NAME} create '{ "issuer": "'${ACCOUNT_NAME}'", "maximum_supply": "1000000000.0000 LND", "can_freeze": 0, "can_recall": 0, "can_whitelist": 0}' -p ${ACCOUNT_NAME}
+exe cleos push action ${CONTRACT_NAME} create '{ "issuer": "'${ACCOUNT_NAME}'", "maximum_supply": "10000.0000 '${token}'", "can_freeze": 0, "can_recall": 0, "can_whitelist": 0}' -p ${ACCOUNT_NAME}
 
 color_printf "Issue some to new currency"
-exef cleos push action ${ACCOUNT_NAME} issue '{ "to": "'${ACCOUNT_NAME}'", "quantity": "10000.0000 LND", "memo": "Initial amount of tokens for you."}' -p ${ACCOUNT_NAME}
+exef cleos push action ${CONTRACT_NAME} issue '{ "to": "'${ACCOUNT_NAME}'", "quantity": "1000.0000 '${token}'", "memo": "Initial amount of tokens for you."}' -p ${ACCOUNT_NAME}
 
-color_printf "Read back the result"
-exef cleos get table ${ACCOUNT_NAME} ${ACCOUNT_NAME} accounts
+color_printf "Issue too much new currency - should fail"
+exe cleos push action ${CONTRACT_NAME} issue '{ "to": "'${ACCOUNT_NAME}'", "quantity": "11000.0000 '${token}'", "memo": "Initial amount of tokens for you."}' -p ${ACCOUNT_NAME}
 
-color_printf "Transfer some tokens"
-exef cleos push action currency transfer '{ "from": "'${ACCOUNT_NAME}'", "to": "eosio", "quantity": "20.0000 LND", "memo": "my first transfer"}' --permission currency@active
+color_printf "Read back the stats"
+exe cleos get currency stats ${CONTRACT_NAME} ${token}
 
-color_printf "Read back the result for currency"
-exef cleos get table ${ACCOUNT_NAME} ${ACCOUNT_NAME} accounts
+color_printf "Transfer some tokens - should succeed"
+exef cleos push action ${CONTRACT_NAME} transfer '{ "from": "'${ACCOUNT_NAME}'", "to": "eosio", "quantity": "500.0000 '${token}'", "memo": "my first transfer"}' --permission ${ACCOUNT_NAME}@active
 
-color_printf "Read back the result for eosio"
-exef cleos get table ${ACCOUNT_NAME} eosio accounts
+color_printf "Read back the result balance - should increase"
+exef cleos get currency balance ${CONTRACT_NAME} ${ACCOUNT_NAME}
